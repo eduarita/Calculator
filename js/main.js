6cf13ba39b05b2d5210cfba70a0f831e900129e6
EduardoCastro26 <educastro2610@gmail.com>
2022-08-13 16:06:31 -0600

    Diseño de calculadora

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-
-function borrar(){
-    document.getElementById("pantalla").value = ``;
+function borrar() {
+  document.getElementById("pantalla").value = ``;
 }
 
-function agregar(id){
-    document.getElementById('pantalla').value += 
-    `${document.getElementById(id).value}`
-
+function agregar(id) {
+  document.getElementById("pantalla").value += `${
+    document.getElementById(id).value
+  }`;
 }
 
-function resultado(){
-    let ans = eval(document.getElementById('pantalla').value)
+function resultado() {
+  let ans = eval(document.getElementById("pantalla").value);
 
-    document.getElementById('pantalla').value = ans
+  document.getElementById("pantalla").value = ans;
 }
